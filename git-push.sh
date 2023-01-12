68716bd30ba043b8a4dd583d7e7b06bc53b93e39
derlappen <48190856+derlappen@users.noreply.github.com>
2023-01-12 20:04:51 +0100

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -3,8 +3,8 @@
 source config.file
 
 git init
-git remote add origin  https://github.com/derlappen/$RepoName.git
+git remote add origin  https://github.com/$username/$RepoName.git
 
 git add .
 git commit -m "commit"
-git push https://$TOKEN@github.com/derlappen/$RepoName.git
+git push https://$TOKEN@github.com/$username/$RepoName.git
