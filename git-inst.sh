858b67db59c2120cc3c51cb7d9244fcf98a47c42
Der Lappen <derlappen@lobusgaming.com>
2023-01-12 18:23:50 +0100

    commit

--- git-inst.sh
+++ git-inst.sh
@@ -1,4 +1,4 @@
-git config –global user.name “derlappen”
-git config –global user.email “der.lappen@gmx.ch”
+
+
 git init
 git remote add origin  https://github.com/derlappen/testrepo.git
--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,9 @@
 TOKEN="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"
 RepoName="testrepo"
 
+git init
+git remote add origin  https://github.com/derlappen/$RepoName.git
+
 git add .
 git commit -m "commit"
 git push https://$TOKEN@github.com/derlappen/$RepoName.git
